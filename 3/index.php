0506506e2ad48527de017417a6703acc2b1ae9f7
Hinagawa <psalimullina@gmail.com>
2020-05-14 16:07:29 +0300

    edit

--- 3/index.php
+++ 3/index.php
@@ -1,29 +1,33 @@
 <?php
 if (isset($_REQUEST['input_string'])){
-    $my_string=$_REQUEST['input_string'];
-    $new_string=explode("\n",$my_string);
-    $new=array(0);
-    $k=0;
-    $i=0;
-    foreach ($new_string as $v) {
-        $new[$i] = explode(" ", $v);
-       shuffle($new[$i]);
-      $new[$i]= implode(" ", $new[$i]);
-      $i++;
+    $my_string = $_REQUEST['input_string'];
+    $my_string = explode("\n", $my_string);
+    $array_sort = array(0);
+    $text_array = array(0);
+    $k = 0;
+    $i = 0;
+    foreach ($my_string as $v) {
+        $text_array[$i] = $v;
+        $i++;
     }
-    foreach ($new as $value) {
-        $array_sort[$k] = explode(" ", $value);
+    foreach ($my_string as $v) {
+        $text_array[$i] = explode(" ", $v);
+        shuffle($text_array[$i]);
+        $text_array[$i] = implode(" ", $text_array[$i]);
+        $i++;
+    }
+    foreach ($text_array as $v) {
+        $array_sort[$k] = explode(" ", $v);
         $k++;
     }
 
-    uasort($array_sort, function ($s1, $s2){
-        return strcmp($s1[1],$s2[1]);
+    uasort($array_sort, function ($s1, $s2) {
+        return strcmp(strtolower($s1[1]), strtolower($s2[1]));
     });
 
-    foreach ($array_sort as $value) {
-        print implode(" ", $value) ."<br>";
+    foreach ($array_sort as $v) {
+        print implode(" ", $v) . "<br>";
     }
-
 }
 else include "index.html";
 ?>
