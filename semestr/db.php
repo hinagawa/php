4829eeb998b08ab9ec1d49fe44c68bf2eea994de
hinagawa <48998084+hinagawa@users.noreply.github.com>
2020-05-28 21:02:09 +0300

    Update db.php

--- semestr/db.php
+++ semestr/db.php
@@ -2,7 +2,7 @@
 
 
 
-    $mysqli = mysqli_connect('localhost', 'root', '1416', 'semestr_php');
+    $mysqli = mysqli_connect('localhost', 'user', 'password', 'semestr_php');
 
     if (!$mysqli) {
         die('Error connect to DataBase');
